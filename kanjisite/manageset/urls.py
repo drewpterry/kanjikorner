6d20ee8d17ed5604a7c7f1d2303311c297f3786c
Drew Terry <drewterry@Drews-MacBook-Pro.local>
2014-06-09 01:03:04 -0700

    added create set page

--- kanjisite/manageset/urls.py
+++ kanjisite/manageset/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
 #url takes four arguments - regex (searches for matching term), view, kwargs, name (naming urls)
     # ex: /profile/
     url(r'^(?P<full_name>\w*)$', views.main_profile, name='index'),
+    url(r'^(?P<full_name>\w*)/new-set$', views.create_new_set, name='newset'),
    
     
 )   
