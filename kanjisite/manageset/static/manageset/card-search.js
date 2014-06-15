8d13cb253502546e62ece83541e4abebc7c6521d
Drew Terry <drewterry@Drews-MacBook-Pro.local>
2014-06-14 23:56:51 -0700

    remove words from bank

--- kanjisite/manageset/static/manageset/card-search.js
+++ kanjisite/manageset/static/manageset/card-search.js
@@ -1,5 +1,4 @@
-// var chosenwords = [1,2,3,4];
-// document.getElementById('chosenwords').value = chosenwords;
+
 
 var filter = "grade";
 
@@ -8,12 +7,11 @@ var search = function(signal){
 	$.ajax({
 		url:'http://localhost:8000/profile/new-set/word-search',
 		type:'GET',
-		// dataType: 'json',
 		data:{theorder: filter , csrfmiddlewaretoken: '{{ csrf_token }}'},
 		success: function(data){
 			
 			data = JSON.parse(data);
-			console.log("workeds");
+			
 			
 			if (signal == ''){
 				keyword = '';
@@ -33,7 +31,7 @@ var search = function(signal){
 						content = content + "<div id = 'meaning'>" + data[i].fields.kanji_meaning + "</div>";
 						content = content + "<div id = 'grade'>" + data[i].fields.grade + "</div>";
 						content = content + "<button class = 'add-remove'>know it!</button>";
-						content = content + "<button class = 'add-remove'>add</button>";
+						content = content + "<button class = 'add-remove' onclick = 'addword(" + data[i].pk + ",\"" + data[i].fields.kanji_name + "\",\"" + data[i].fields.kanji_meaning + "\", this)'>add</button>";
 						content = content + "</div></div>"
 						content = content + "<div class = 'back'>" + data[i].fields.kanji_name + "</div></div></div>";
 						
@@ -52,7 +50,8 @@ var search = function(signal){
 						content = content + "<div id = 'meaning'>" + data[i].fields.kanji_meaning + "</div>";
 						content = content + "<div id = 'grade'>" + data[i].fields.grade + "</div>";
 						content = content + "<button class = 'add-remove'>know it!</button>";
-						content = content + "<button class = 'add-remove'>add</button>";
+						content = content + "<button class = 'add-remove'"; 
+						content = content + "onclick = 'addword(" + data[i].pk + ",\"" + data[i].fields.kanji_name + "\",\"" + data[i].fields.kanji_meaning + "\")'>add</button>";
 						content = content + "</div></div>"
 						content = content + "<div class = 'back'>" + data[i].fields.kanji_name + "</div></div></div>";
 						foundflag = true;
@@ -78,6 +77,33 @@ var search = function(signal){
 	});
 };
 
+
+var addword = function(idnumber, kanji, meaning, element){
+	console.log("worded");
+	console.log(element.innerText);
+	if(element.innerText == "add"){
+		var hiddeninput = "<input type = 'hidden' id = 'chosenwords" + idnumber + "' class = 'text-area left-margin' name = 'chosenwords' value = '" + idnumber +"' ></input>";
+		$("#create-set-form").prepend(hiddeninput); 
+	
+		var minicard =''; 
+		minicard = minicard + "<div id = 'answercontainer" + idnumber + "' class = 'answerbox mini'>";
+		minicard = minicard + "<div class = 'flipper'><div class = 'front mini'>"; 
+		minicard = minicard + "<div id = 'kanji'>" + kanji + "</div>";
+		minicard = minicard + "<div id = 'meaning'>" + meaning + "</div>";
+		minicard = minicard + "</div></div>"
+		minicard = minicard + "<div class = 'back mini'>" + kanji + "</div></div></div>";
+	
+		$("#wordlist").prepend(minicard);
+		element.parentNode.className += " outline";
+	
+		element.innerHTML = "remove";
+	}else{
+		element.innerHTML = "add";
+		element.parentNode.className = "front";
+		document.getElementById('answercontainer'+idnumber).remove();
+		document.getElementById('chosenwords'+idnumber).remove();
+	}
+}
 
 document.getElementById('strokes').onclick = function(){
 	filter = "strokes";
