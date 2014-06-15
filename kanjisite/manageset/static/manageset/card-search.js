adc92553e625865f5217d04f4565dc6bfb5bf61e
Drew Terry <drewterry@Drews-MacBook-Pro.local>
2014-06-15 15:15:18 -0700

    remove words from stack

--- kanjisite/manageset/static/manageset/card-search.js
+++ kanjisite/manageset/static/manageset/card-search.js
@@ -1,6 +1,7 @@
 
 
 var filter = "grade";
+var addcheck = [];
 
 var search = function(signal){
 	
@@ -17,7 +18,7 @@ var search = function(signal){
 				keyword = '';
 			}else{
 			keyword = document.getElementById('searchbox').value;
-			}
+			};
 			
 			var foundflag = false;
 			var content = "";
@@ -25,7 +26,7 @@ var search = function(signal){
 			if(keyword == ''){
 				for(var i= 0; i<data.length; i++){
 					
-						content = content + "<div id = 'answercontainer' class = 'answerbox' >";
+						content = content + "<div id = 'answercontainer" + data[i].pk + "' class = 'answerbox' >";
 						content = content + "<div class = 'flipper'><div class = front>"; 
 						content = content + "<div id = 'kanji'>" + data[i].fields.kanji_name + "</div>";
 						content = content + "<div id = 'meaning'>" + data[i].fields.kanji_meaning + "</div>";
@@ -35,8 +36,9 @@ var search = function(signal){
 						content = content + "</div></div>"
 						content = content + "<div class = 'back'>" + data[i].fields.kanji_name + "</div></div></div>";
 						
-					}
+					};
 				document.getElementById('container').innerHTML = content;	
+				
 						
 			}else{
 				
@@ -44,32 +46,43 @@ var search = function(signal){
 					
 					if(data[i].fields.kanji_meaning.toLowerCase().indexOf(keyword.toLowerCase())!= -1||data[i].fields.kanji_name.indexOf(keyword)!=-1){
 				
-						content = content + "<div id = 'answercontainer' class = 'answerbox' onclick = 'clicked(this)'>";
+						content = content + "<div id = 'answercontainer" + data[i].pk + "' class = 'answerbox' onclick = 'clicked(this)'>";
 						content = content + "<div class = 'flipper'><div class = front>"; 
 						content = content + "<div id = 'kanji'>" + data[i].fields.kanji_name + "</div>";
 						content = content + "<div id = 'meaning'>" + data[i].fields.kanji_meaning + "</div>";
 						content = content + "<div id = 'grade'>" + data[i].fields.grade + "</div>";
 						content = content + "<button class = 'add-remove'>know it!</button>";
 						content = content + "<button class = 'add-remove'"; 
-						content = content + "onclick = 'addword(" + data[i].pk + ",\"" + data[i].fields.kanji_name + "\",\"" + data[i].fields.kanji_meaning + "\")'>add</button>";
+						content = content + "onclick = 'addword(" + data[i].pk + ",\"" + data[i].fields.kanji_name + "\",\"" + data[i].fields.kanji_meaning + "\", this)'>add</button>";
 						content = content + "</div></div>"
 						content = content + "<div class = 'back'>" + data[i].fields.kanji_name + "</div></div></div>";
 						foundflag = true;
-					}
+							
+					};
 					
 					if(foundflag == true){
+						
+						console.log('works');
 						document.getElementById('container').innerHTML = content;
+						
 					}else{
 						document.getElementById('container').innerHTML = "There are no matching results for your query: " + keyword ;
-					}
-						
-					
+					};
 					
 				}
+				
 			}
 				// document.getElementById('container').innerHTML = content;
-			
-			
+				
+					for(var i = 0; i<addcheck.length; i++){
+						if(document.getElementById('answercontainer'+addcheck[i]) != null){
+							
+							//add class to div where class = front
+							document.getElementById('answercontainer'+addcheck[i]).firstChild.firstChild.className += " outline";
+							//change add button to a remove button
+							document.getElementById('answercontainer'+addcheck[i]).firstChild.firstChild.lastChild.innerHTML = "remove";
+						}
+					}	
 		},
 		failure: function(data){
 			alert("Sorry got an error")
@@ -79,14 +92,13 @@ var search = function(signal){
 
 
 var addword = function(idnumber, kanji, meaning, element){
-	console.log("worded");
-	console.log(element.innerText);
+	
 	if(element.innerText == "add"){
 		var hiddeninput = "<input type = 'hidden' id = 'chosenwords" + idnumber + "' class = 'text-area left-margin' name = 'chosenwords' value = '" + idnumber +"' ></input>";
 		$("#create-set-form").prepend(hiddeninput); 
 	
 		var minicard =''; 
-		minicard = minicard + "<div id = 'answercontainer" + idnumber + "' class = 'answerbox mini'>";
+		minicard = minicard + "<div id = 'answercontainerdif" + idnumber + "' class = 'answerbox mini'>";
 		minicard = minicard + "<div class = 'flipper'><div class = 'front mini'>"; 
 		minicard = minicard + "<div id = 'kanji'>" + kanji + "</div>";
 		minicard = minicard + "<div id = 'meaning'>" + meaning + "</div>";
@@ -94,31 +106,42 @@ var addword = function(idnumber, kanji, meaning, element){
 		minicard = minicard + "<div class = 'back mini'>" + kanji + "</div></div></div>";
 	
 		$("#wordlist").prepend(minicard);
+		
+		addcheck.push(idnumber);
+		
+		// permanently add outline to added card or removes it if remove is clicked
 		element.parentNode.className += " outline";
-	
 		element.innerHTML = "remove";
 	}else{
 		element.innerHTML = "add";
 		element.parentNode.className = "front";
-		document.getElementById('answercontainer'+idnumber).remove();
+		//removes minibox
+		document.getElementById('answercontainerdif'+idnumber).remove();
+		//removes hidden field
 		document.getElementById('chosenwords'+idnumber).remove();
-	}
-}
+		//removing id from array
+		var position = addcheck.indexOf(idnumber);
+		addcheck.splice(position,1);
+		
+	};
+};
+
+
 
 document.getElementById('strokes').onclick = function(){
 	filter = "strokes";
-	search('');
-}
+	search('yes');
+};
 
 document.getElementById('gradefilter').onclick = function(){
 	filter = "grade"
 	search("yes");
-}
+};
 
 document.getElementById('searchbox').onkeyup = function(){
 	// filter = "grade"
 	search("yes");
-}
+};
 
 search('');
 		
