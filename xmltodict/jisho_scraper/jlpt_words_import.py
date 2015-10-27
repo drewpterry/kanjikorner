e1461f90f78de1b8418e47068b846ea792731623
Drew Terry <drewpterry@yahoo.com>
2015-10-26 22:14:15 -0700

    settings

--- xmltodict/jisho_scraper/jlpt_words_import.py
+++ xmltodict/jisho_scraper/jlpt_words_import.py
@@ -7,31 +7,35 @@ from bs4 import BeautifulSoup
 import csv
 import sys
 
-with open('jlpt_level_export.csv','r') as csvfile:
+with open('jlpt_level_export2.csv','r') as csvfile:
     reader = csv.reader(csvfile, delimiter=';')
     count = 0
     for row in reader:
+        print row[1], row[2], row[11], count
+        # # print count, row[0]
+        #
         count = count +1
-        if row[1] != "none" and row[0] != "id":
+        if row[1] != "none" and row[0] != "id" and count >75988:
             # encoded_word = row[2].decode('utf-8')
             # print row[2], type(int(row[4]))
-            # word = Words.objects.filter(real_word = encoded_word, meaning = row[3])
-            word = Words.objects.get(id = row[0])
-            word.jlpt_level = row[1]
+            word = Words.objects.filter(real_word = row[1], meaning = row[2])
+            # word = Words.objects.get(id = row[0])
+            # word.jlpt_level = row[1]
             # print row[1], "hello"
-            # for each in word:
-            
+            for each in word:
+
                 # if row[1] == "n/a":
-#
-#                     each.jlpt_level = 7
-#                 elif row[1] == "error":
-#                     each.jlpt_level = 6
-#                 else:
-                    # each.jlpt_level = row[1]
-                    
-            
-                # each.save()
-            print count, row[0], word.jlpt_level
+ #
+ #                    each.jlpt_level = 7
+ #                elif row[1] == "error":
+ #                    each.jlpt_level = 6
+ #                else:
+ #                    each.jlpt_level = row[1]
+                 each.jlpt_level = row[11]
+
+
+                 each.save()
+                 # print count, row[0], each.jlpt_level
         # word = Words.objects.get(row)
-        # print row
+#         print row
     
