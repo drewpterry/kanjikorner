38175097dd32f061d69cb03361446df9a94fdfa6
Drew Terry <drewpsterry@gmail.com>
2016-12-04 22:25:27 +0900

    remove unneeded urls

--- flashcard/urls.py
+++ flashcard/urls.py
@@ -3,13 +3,6 @@ from flashcard.views import *
 
 urlpatterns =[ 
 #url takes four arguments - regex (searches for matching term), view, kwargs, name (naming urls)
-    url(r'^(?P<full_name>\w*)/(?P<set_name>[-\w ]+)/practice$', practice_stack, name='practice-stack'),
-    url(r'^(?P<full_name>\w*)/(?P<set_name>[-\w ]+)/complete-stack$', complete_stack, name='complete-stack'),
-    url(r'^(?P<full_name>\w*)/SRS-review$', srs_review_words, name='srs_review_words'),
-    url(r'^(?P<full_name>\w*)/tier-level-update$', tier_level_update, name='tier_level_update'),
-
-
-
     url(r'^srs$', view_srs_review, name='view-srs-review'),
     url(r'^srs/get$', get_srs_review, name='get-srs-review'),
     url(r'^update_word$', update_review_word, name='update_review_word'),
