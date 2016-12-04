915be1348e607dd6937feaf83540ac541a275486
Drew Terry <drewpsterry@gmail.com>
2016-12-04 22:18:58 +0900

    add homepage test

--- homepage/tests.py
+++ homepage/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+import unittest
+from django.test import Client
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class HomePageTest(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_get_index(self):
+        # Issue a GET request.
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'homepage/index.html')
+
+        self.user = User.objects.create_user(username='testuser', password='12345') 
+        self.user.save() 
+        login = self.client.login(username='testuser', password='12345')
+
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'manageset/dashboard.html')
