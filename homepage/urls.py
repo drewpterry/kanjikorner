6ead00c0cfaf93710622d33b7b61cd3f31c4943c
Drew Terry <drewpsterry@gmail.com>
2017-02-04 23:15:24 +0900

    full removal of homepage app

--- homepage/urls.py
+++
@@ -1,9 +0,0 @@
-#homepage
-from django.conf.urls import patterns, url
-
-from homepage import views
-
-urlpatterns = [ 
-#url takes four arguments - regex (searches for matching term), view, kwargs, name (naming urls)
-    # ex: /login/
-]
--- kanjisite/settings/base.py
+++ kanjisite/settings/base.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'homepage',
     'manageset',
     'flashcard',
     'admin_data_collection',
