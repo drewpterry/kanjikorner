2e885d8eb07e8ff1a25599335a513e13e523c75b
Drew Terry <drewpsterry@gmail.com>
2017-04-15 21:03:41 +0900

    reformat fields in admin

--- manageset/admin.py
+++ manageset/admin.py
@@ -6,20 +6,49 @@ from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
 class UserResource(resources.ModelResource):
-
     class Meta:
         model = User
 
 class UserAdmin(ImportExportModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_active', 'date_joined', 'is_staff', 'userprofile_foo')
+    list_display = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'is_active',
+            'date_joined',
+            'is_staff',
+            'userprofile_foo'
+            )
+
     def userprofile_foo(self, x):
             return x.userprofile.most_words_practiced_in_day
     resource_class = UserResource
            
 class SetsAdmin(admin.ModelAdmin):
-    list_display = ('name','description', 'master_order', 'level', 'sub_level')
-    list_editable = ('name','description', 'master_order', 'level', 'sub_level')
-    fields = ('name','description', 'words', 'kanji', 'master_order', 'level', 'sub_level')
+    list_display = (
+            'name',
+            'description',
+            'master_order',
+            'level',
+            'sub_level'
+            )
+    list_editable = (
+            'name',
+            'description',
+            'master_order',
+            'level',
+            'sub_level'
+            )
+    fields = (
+            'name',
+            'description',
+            'words',
+            'kanji',
+            'master_order',
+            'level',
+            'sub_level'
+            )
 
 class MeaningsInline(admin.TabularInline):
     model = WordMeanings
@@ -28,14 +57,41 @@ class WordQuestionInline(admin.TabularInline):
     model = WordQuestion
 
 class WordsResource(resources.ModelResource):
-
     class Meta:
         model = Words
 
 class WordsAdmin(ImportExportModelAdmin):
-    list_display = ('id', 'master_order', 'real_word' ,'meaning', 'hiragana', 'combined_frequency', 'frequency_thousand', 'published', 'master_order')
-    list_editable = ('combined_frequency', 'frequency_thousand', 'published', 'master_order')
-    fields = ('real_word', 'meaning', 'hiragana', 'frequency', 'frequency_two', 'combined_frequency', 'frequency_thousand','part_of_speech','published','duplicate_word', 'master_order', 'kanji')
+    list_display = (
+            'id',
+            'master_order',
+            'real_word',
+            'meaning',
+            'hiragana',
+            'combined_frequency',
+            'frequency_thousand',
+            'published',
+            'master_order'
+            )
+    list_editable = (
+            'combined_frequency',
+            'frequency_thousand',
+            'published',
+            'master_order'
+            )
+    fields = (
+            'real_word',
+            'meaning',
+            'hiragana',
+            'frequency',
+            'frequency_two',
+            'combined_frequency',
+            'frequency_thousand',
+            'part_of_speech',
+            'published',
+            'duplicate_word',
+            'master_order',
+            'kanji'
+            )
     list_filter = ('published',)
     inlines = [
         MeaningsInline,
@@ -55,7 +111,22 @@ class KanjiResource(resources.ModelResource):
 
 class KanjiAdmin(ImportExportModelAdmin):
     fields = ('kanji_name', 'kanji_meaning','readings', 'strokes', 'grade', 'master_order')
-    list_display = ('kanji_name', 'kanji_meaning','readings', 'strokes', 'on_kun_readings', 'grade', 'jlpt_level', 'newspaper_frequency', 'jinmeiyo', 'twitter_frequency', 'aozora_frequency', 'news_frequency', 'wikipedia_frequency', 'master_order')
+    list_display = (
+            'kanji_name',
+            'kanji_meaning',
+            'readings',
+            'strokes',
+            'on_kun_readings',
+            'grade',
+            'jlpt_level',
+            'newspaper_frequency',
+            'jinmeiyo',
+            'twitter_frequency',
+            'aozora_frequency',
+            'news_frequency',
+            'wikipedia_frequency',
+            'master_order'
+            )
     list_editable = ('kanji_meaning', 'master_order')
     list_filter = ('jlpt_level',)
     search_fields = ('kanji_name', 'kanji_meaning')
@@ -79,7 +150,6 @@ class SentenceAdmin(ImportExportModelAdmin):
     resource_class = SentenceResource
     pass
 
-
 admin.site.register(Sets, SetsAdmin)
 admin.site.register(Words, WordsAdmin)
 admin.site.register(Kanji, KanjiAdmin)
@@ -87,4 +157,3 @@ admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.register(Sentence, SentenceAdmin)
-# admin.site.register(WordMeanings, WordMeaningsAdmin)
